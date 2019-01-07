34f975a7eab13d811f0056187ebee4ca2b66f066
Will Holt <halbrd@gmail.com>
2019-01-07 21:38:57 +1100

    make filetype check more specific

--- organize_screenshots.py
+++ organize_screenshots.py
@@ -13,7 +13,7 @@ def list_images(dir):
     r = []
     for root, dirs, files in os.walk(dir):
         for name in files:
-            if "jpg" in name or "mp4" in name or "png" in name:
+            if name.endswith('jpg') or name.endswith('mp4') or name.endswith('png'):
                 r.append(os.path.join(root, name))
     return r
 
